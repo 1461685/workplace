9c668b59b5ea324041204191b8069b0ec8f2b775
1461685 <nguyettram10@gmail.com>
2021-05-31 16:29:39 +0700

    complete react practice project

--- react-complete-guide-practice/src/App.js
+++ react-complete-guide-practice/src/App.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 
+import AddUser from './components/Users/AddUser';
+import UserList from './components/Users/UserList';
 
-function App() {
+function App()
+{
+  const [userList, setUserList] = useState([]);
+
+  const addUserHandler = (uName, uAge) =>
+  {
+    setUserList((prevUserList) =>
+    {
+      return (
+        [...prevUserList,
+          { name: uName, age: uAge, id: Math.random().toString() }
+        ]);
+    });
+  };
+
   return (
     <div>
-
+      <AddUser onAddUser={addUserHandler} />
+      <UserList users={userList} />
     </div>
   );
 }
